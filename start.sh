5b3020820e0289c753e1b8ecdc864137685ba87d
Nathan Nguyen <31893431+Doomking36@users.noreply.github.com>
2023-05-06 14:43:19 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,6 +4,7 @@ yes | pacman -Sy tar curl xz
 curl --output kiss.xz https://codeberg.org/attachments/7f91bc9e-6fb1-481f-bacd-95798fbf298c
 tar xf kiss.xz -C /mnt
 cp chroot.sh /mnt
+cp profile /mnt
 
 /mnt/bin/kiss-chroot /mnt << EOT
 ./chroot.sh
