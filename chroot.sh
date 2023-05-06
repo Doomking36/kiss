d9236acf4e4593a43f7e1e65edda3ea88c24d691
Nathan Nguyen <31893431+Doomking36@users.noreply.github.com>
2023-05-06 14:14:30 -0500

    Update chroot.sh

--- chroot.sh
+++ chroot.sh
@@ -1,20 +1,13 @@
 #!/bin/sh
 
 mkdir -p /home/dk/repos
+mv profile /home/dk
 cd /home/dk
+mv profile ~/.profile
 
 git clone https://github.com/kiss-community/repo /home/dk/repos/repo
 git clone https://github.com/kiss-community/community /home/dk/repos/community
 git clone https://github.com/ehawkvu/kiss-xorg /home/dk/repos/xorg
-
-echo "KISS_PATH=/home/dk/repos/repo/core" >> ~/.profile
-echo "KISS_PATH=$KISS_PATH:/home/dk/repos/repo/extra" >> ~/.profile
-echo "KISS_PATH=$KISS_PATH:/home/dk/repos/repo/wayland" >> ~/.profile
-echo "KISS_PATH=$KISS_PATH:/home/dk/repos/xorg/extra" >> ~/.profile
-echo "KISS_PATH=$KISS_PATH:/home/dk/repos/xorg/xorg" >> ~/.profile
-echo "KISS_PATH=$KISS_PATH:/home/dk/repos/xorg/community" >> ~/.profile
-echo "KISS_PATH=$KISS_PATH:/home/dk/repos/community/community" >> ~/.profile
-echo "export KISS_PATH" >> ~/.profile
 
 . ~/.profile
 
