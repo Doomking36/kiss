32b4b8b4a040c14ed334dab97cd7b881bbe014e9
Nathan Nguyen <31893431+Doomking36@users.noreply.github.com>
2023-05-07 01:23:00 -0500

    Update chroot.sh

--- chroot.sh
+++ chroot.sh
@@ -43,6 +43,7 @@ git clone https://github.com/Doomking36/kiss-setup
 cp kiss-setup/profile /home/dk
 mv profile ~/.profile
 EOT
+
 curl -fLO https://github.com/cemkeylan/genfstab/raw/master/genfstab
 chmod +x genfstab
 ./genfstab -U / >> /etc/fstab
